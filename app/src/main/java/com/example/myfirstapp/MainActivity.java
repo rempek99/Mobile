38cef722f036a68c1a94869873c6c4b53ac11ad3
rempek99 <arkadiusz.remplewicz@gmail.com>
2020-10-09 18:41:14 +0200

    exercise 1

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myfirstapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,6 +13,9 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +34,79 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button addBtn = (Button) findViewById(R.id.AddBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumber = (EditText) findViewById(R.id.Num1EditText);
+                EditText secondNumber = (EditText) findViewById(R.id.Num2EditText);
+                if(firstNumber.getText().toString().isEmpty() || secondNumber.getText().toString().isEmpty())
+                    return;
+
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+                float result = Float.parseFloat(firstNumber.getText().toString()) + Float.parseFloat(secondNumber.getText().toString());
+                resultTextView.setText(String.valueOf(result));
+            }
+        });
+
+        Button computeBtn = (Button) findViewById(R.id.computeBtn);
+        computeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstFactor = (EditText) findViewById(R.id.factor1EditText);
+                EditText secondFactor = (EditText) findViewById(R.id.factor2EditText);
+                EditText thirdFactor = (EditText) findViewById(R.id.factor3EditText);
+                if(firstFactor.getText().toString().isEmpty() || secondFactor.getText().toString().isEmpty() || thirdFactor.getText().toString().isEmpty())
+                    return;
+                Resources res = getResources();
+                TextView resultTextView = (TextView) findViewById(R.id.equalResultTextView);
+                float fct1 = Float.parseFloat(firstFactor.getText().toString());
+                float fct2 = Float.parseFloat(secondFactor.getText().toString());
+                float fct3 = Float.parseFloat(thirdFactor.getText().toString());
+                String result = "";
+                if(fct1==0)
+                {
+                    if(fct2==0)
+                    {
+                        //funkcja stala
+                        result = res.getString(R.string.result1) + " 0 = 0";
+                        resultTextView.setText(result);
+                        if(fct3!=0) {
+                            result = res.getString(R.string.result1) + " " + res.getString(R.string.conflict) + " " + String.valueOf(fct3) + " \u2260 0";
+                        }
+                    }
+                    else {
+                        //funkcja linionwa
+                        float root = -fct3 / fct2;
+                        result = res.getString(R.string.result2) + String.valueOf(root);
+                    }
+                }
+                else {
+                    //funkcja kwadratowa
+                    double delta, root1, root2;
+                    delta = fct2*fct2 - (4 * fct1 * fct3);
+                    if(delta<0)
+                    {
+                        result = res.getString(R.string.result3) + String.valueOf(delta);
+                    }
+                    if(delta == 0)
+                    {
+                        root1 = -fct2 / (2 * fct1);
+                        result = res.getString(R.string.result4a) + String.valueOf(root1) + res.getString(R.string.result4b) + String.valueOf(delta);
+                    }
+                    if(delta>0)
+                    {
+                        root1 = (-fct2 - Math.sqrt(delta) ) / (2 * fct2);
+                        root2 = (-fct2 + Math.sqrt(delta) ) / (2 * fct2);
+                        result = res.getString(R.string.result5a) + String.valueOf(root1) + res.getString(R.string.result5b) + String.valueOf(root2)
+                        + res.getString(R.string.result5c) + String.valueOf(delta);
+                    }
+                }
+                resultTextView.setText(result);
+            }
+        });
+
     }
 
     @Override
